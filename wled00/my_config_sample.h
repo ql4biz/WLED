cf7e6e951935eb327cf1f08022df3c81bf9a2c70
Carlo D'Ambrosio <carlo@ql4b.net>
2024-09-25 17:36:21 +0200

    chore: my_config sample file

--- wled00/my_config_sample.h
+++ wled00/my_config_sample.h
@@ -14,13 +14,31 @@
  */
 
 // uncomment to force the compiler to show a warning to confirm that this file is included
-//#warning **** my_config.h: Settings from this file are honored ****
+#warning **** my_config.h: Settings from this file are honored ****
 
-/* Uncomment to use your WIFI settings as defaults
-  //WARNING: this will hardcode these as the default even after a factory reset
-#define CLIENT_SSID "Your_SSID"
-#define CLIENT_PASS "Your_Password"
-*/
+//WARNING: this will hardcode these as the default even after a factory reset
+#define CLIENT_SSID       "QL4B-MAIN"
+#define CLIENT_PASS       "CMmjPrmg3CqHMHbPwtm3"
 
-//#define MAX_LEDS 1500       // Maximum total LEDs. More than 1500 might create a low memory situation on ESP8266.
-//#define MDNS_NAME "wled"    // mDNS hostname, ie: *.local
+#define MDNS_NAME         "wled8"               // mDNS hostname, ie: *.local
+
+
+#define MQTT_ENABLED                            // Enable MQTT
+
+// MQTT Configuration
+#define MQTT_MAX_TOPIC_LEN  32
+#define MQTT_MAX_SERVER_LEN 32
+
+// Configure MQTT credentials and settings
+#define MQTT_SERVER         "wled8.ql48.net"    // Replace with your MQTT server IP or domain
+#define MQTT_PORT           1883                // Default MQTT port (can be changed if needed)
+#define MQTT_USER           "wled8"             // Optional MQTT username
+#define MQTT_PASS           "ql48"              // Optional MQTT password
+#define MQTT_CLIENT_ID      "wled8"             // Unique client ID for your WLED instance
+#define MQTT_DEVICE_TOPIC   "Ql48/WLEDS/wled7"  // Topic for this specific device
+#define MQTT_GROUP_TOPIC    "Ql48/WLEDS/all"    // Topic for group commands
+#define MQTT_RETAIN         false               // Retain MQTT messages or not
+
+// Led Configuration
+#define LEDPIN 2
+
